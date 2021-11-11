4a8d6a3e89dbeb7ba6a93d2c8294f3f5a3fea009
Franklamps <franklamp14@gmail.com>
2021-11-11 23:00:07 +0000

    edited the homepage.dart

--- cmaminiproject/lib/homepage.dart
+++ cmaminiproject/lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 
@@ -12,6 +14,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List todos =[];
+  String input ="";
+
+  createTodos(){
+    DocumentReference documentReference = FirebaseFirestore.instance.collection("MYTODOS").doc(input);
+
+    //Map
+    Map<String, String> todos= {"todoTile":input};
+
+    documentReference.set(todos).whenComplete((){
+      print("$input created");
+    });
+  }
+
+  deleteTodos(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,9 +44,43 @@ class _HomePageState extends State<HomePage> {
           ),),
         centerTitle: true,
       ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        showDialog(context: context, builder:(BuildContext context){
+          return AlertDialog(title:const Text("Add TodoList"),
+          content: TextField(onChanged:(String value){
+            input = value;
+          }),
+          actions: <Widget>[
+            FlatButton(onPressed: (){
+              setState(() {
+                todos.add(input);
+              });
+              Navigator.pop(context);
+            }, child: const Text("add")),
+          ],);
+        });
+      },
+      child: const Icon(Icons.add,),),
 
-      //body: ,
-
+      body:StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("MyTodos").snapshots(),
+    builder: (context, snapshots) {
+      return ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(key: Key(todos[index]), child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
+              margin: const EdgeInsets.all(16),
+              elevation: 4,
+              child: ListTile(
+                  title: Text(todos[index]),
+                  trailing: IconButton(onPressed: () {},
+                    icon: const Icon(Icons.delete),
+                    color: Colors.redAccent,)
+              ),));
+          });
+    })
     );
   }
 }
--- cmaminiproject/lib/main.dart
+++ cmaminiproject/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       initialRoute: "/",
       routes: {
-        "/":(context) =>const LoginPage(),
+        "/":(context) =>const HomePage(),
         "/Login":(context) => const LoginPage(),
         "/Home":(context) =>const HomePage(),
 
